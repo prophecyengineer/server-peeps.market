3712fad434d47286f3f16420c090400f4ff7dfa5
lott-e <hi.lottie@icloud.com>
2022-11-27 16:36:03 +0100

    Deploy Medusa Server on Heroku

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,7 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,http://10.0.0.4:7001";
+const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,http://10.0.0.4:7001,http://0.0.0.0:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
@@ -56,19 +56,34 @@ const plugins = [
   // },
 ];
 
+// module.exports = {
+//   projectConfig: {
+//     // cli_migration_dirs: ['node_modules/medusa-marketplace/dist/**/*.migration.js'],
+
+//     // 
+//     // redis_url: REDIS_URL,
+//     // For more production-like environment install PostgresQL
+//     // database_url: DATABASE_URL,
+//     // database_type: "postgres",
+//     database_database: "./medusa-db.sql",
+//     database_type: "sqlite",
+//     store_cors: STORE_CORS,
+//     admin_cors: ADMIN_CORS,
+//   },
+//   plugins,
+// };
+
 module.exports = {
   projectConfig: {
-    // cli_migration_dirs: ['node_modules/medusa-marketplace/dist/**/*.migration.js'],
-
-    // 
-    // redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
-    // database_url: DATABASE_URL,
-    // database_type: "postgres",
-    database_database: "./medusa-db.sql",
-    database_type: "sqlite",
+    redis_url: REDIS_URL,
+    database_url: DATABASE_URL,
+    database_type: "postgres",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
+    database_extra:
+      process.env.NODE_ENV !== "development"
+        ? { ssl: { rejectUnauthorized: false } }
+        : {},
   },
   plugins,
 };
