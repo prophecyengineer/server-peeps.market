3a95b38d2b37eec2ad67308b7fbeef8de8b7afba
lott-e <hi.lottie@icloud.com>
2022-11-27 16:48:11 +0100

    cors

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,7 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,http://10.0.0.4:7001,http://0.0.0.0:7001";
+const ADMIN_CORS = "http://localhost:7000,http://localhost:7001,http://10.0.0.4:7001,http://0.0.0.0:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
