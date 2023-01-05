404affcea5901c9568acd7dcf7cd247329905226
lott-e <hi.lottie@icloud.com>
2023-01-05 20:34:18 +0100

    new seed

--- develop.sh
+++ develop.sh
@@ -2,7 +2,7 @@
 
 #Run migrations to ensure the database is updated
 medusa migrations run
-# medusa user -e some@email.com -p some-password
+medusa user -e some@email.com -p some-password
 medusa seed -f ./data/seed.json
 # ./node_modules/.bin/medex m --run
 #Start development environment
