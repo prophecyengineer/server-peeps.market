fb477d920af16382ae98cf3385bf03e1ba4ed57f
lott-e <hi.lottie@icloud.com>
2023-01-05 20:01:26 +0100

    new seed file

--- develop.sh
+++ develop.sh
@@ -3,7 +3,7 @@
 #Run migrations to ensure the database is updated
 medusa migrations run
 # medusa user -e some@email.com -p some-password
-# medusa seed -f ./data/seed.json
+medusa seed -f ./data/seed.json
 # ./node_modules/.bin/medex m --run
 #Start development environment
 medusa develop
